f9748f29d73ef9a103b5e1e1982a6d4beecf8da5
Yusuf Tasci <yusuftasci013@gmail.com>
2026-05-31 23:51:32 +0200

    app.php test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,24 +12,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // Dit dwingt Laravel om de proxy van Railway te accepteren
         $middleware->trustProxies(at: '*');
-        
-        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_AWS_ELB | 
-            Request::HEADER_X_FORWARDED_FOR | 
-            Request::HEADER_X_FORWARDED_HOST | 
-            Request::HEADER_X_FORWARDED_PORT | 
+
+        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_AWS_ELB |
+            Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
             Request::HEADER_X_FORWARDED_PROTO
         );
 
-        // Jouw bestaande admin alias
         $middleware->alias([
             'admin' => \App\Http\Middleware\IsAdmin::class,
-        ]);
-
-        // De nieuwe beveiligingslaag voor de hele website
-        $middleware->web(append: [
-            \App\Http\Middleware\SitePasswordProtection::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
